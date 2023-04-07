33cd97b48098e3ae76d81b396249a5898c2f7b9e
RayGethers <>
2023-04-07 21:40:14 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,43 @@
+// PROBLEM 1: EVEN OR ODD LOOP
+// - - - - - - - - - - - - - - -
+// var max: Int = 100
+// for num in 1...max {
+//   if num % 2 == 0 {
+//     print("\(num) = EVEN")
+//   } else {
+//     print("\(num) = ODD")
+//   }
+// }
+
+// PROBLEM 2: PRIME NUMBERS
+// var isPrime = true
+// // print("Please enter a number: ")
+// var number = 13
+
+// for num in 2...number - 1 {
+//   if number % num == 0 {
+//     isPrime = false
+//     break
+//   }
+//  print(isPrime)
+// }
+// print("is \(number) prime? : \(isPrime)")
+
+// PROBLEM 3:LOOPING THROUGH STRINGS
+// var words = "This is a test"
+// var total = 0
+// print(words)
+// for letters in words {
+//   if letters == " " {
+//     continue
+//   }
+//   total += 1
+// }
+// print("Total number of letters(excluding spaces): \(total)")
+
+// PROBLEM 4: STARS
+// var star = ""
+// for _ in 1...4 {
+//   star += "* "
+//   print(star)
+// }
